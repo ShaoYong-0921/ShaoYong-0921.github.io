a8fb17607fba3d56a47c4c66e2bf641b914a925b
ShaoYong <henry934629@gmail.com>
2026-07-09 16:45:33 +0800

    Point site URL to shaoyong.dev (Issue #5)

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://shaoyong-0921.github.io/",
+	site: "https://shaoyong.dev/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
