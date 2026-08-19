38035d330e0efa9daacf36244a8c5d5676aae18e
ShaoYong <henry934629@gmail.com>
2026-08-19 10:13:07 +0800

    Revert "換上大橘為重 banner"
    This reverts commit 82f31fe86bbfab4873ae36daf5b12b53137eb043.

--- src/config.ts
+++ src/config.ts
@@ -17,11 +17,11 @@ export const siteConfig: SiteConfig = {
 	},
 	banner: {
 		enable: true,
-		src: "assets/images/cat-banner.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: true, // Display the credit text of the banner image
-			text: "大橘為重", // Credit text to be displayed
+			text: "空色天絵 / NEO TOKYO NOIR 01", // Credit text to be displayed
 			url: "", // (Optional) URL link to the original artwork or artist's page
 		},
 	},
